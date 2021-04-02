48273175a4015dd2ef3b89441767b4744939e102
Neil Gardner <neilgardner1963@gmail.com>
2021-04-02 19:56:06 +0100

    project setup

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -1,5 +1,23 @@
 pragma solidity ^0.5.0;
 
 contract ToDoList {
+	uint public taskCount 0;
 	
+	struct Task {
+		uint id;
+		string content;
+		bool completed;
+	}
+
+	constructor()  public {
+		createTask("Buy some milk");
+	}
+
+	mapping(uint => Task) public tasks;
+
+	function createTask(string memory _content) public {
+		taskCount++;
+		tasks[taskCount] = new Task(taskCount, content, false);
+	}
+
 }
